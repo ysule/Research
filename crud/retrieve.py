70b563c9e3aa08dc7e207dc599bff8f06728e05b
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-05-18 16:17:00 +0530

    nothing

--- crud/retrieve.py
+++ crud/retrieve.py
@@ -6,24 +6,35 @@ import requests
 import urllib
 import time
 
-#function to get data according to url and term
-def search(url, field, term):
-    query = json.dumps({"query":{"match":{field:term}}})
-    response = requests.get(url, data=query)
-    results = json.loads(response.text)
-    return results
+print 'if you want to search by id enter 1'
+print 'if you want to search by key words i.e:all matching items enter 2'
 
-#function to format the data prettily
-def format(results):
-    data = [doc for doc in results['hits']['hits']]
-    for doc in data:
-        print("%s" % (doc['_source']))
+a = int(input("Enter your option: "))
+if a==2:
+	#function to get data according to url and term
+	def search(url, field, term):
+	    query = json.dumps({"query":{"match":{field:term}}})
+	    response = requests.get(url, data=query)
+	    results = json.loads(response.text)
+	    return results
+
+	#function to format the data prettily
+	def format(results):
+	    data = [doc for doc in results['hits']['hits']]
+	    for doc in data:
+	        print("%s" % (doc['_source']))
 
 
-url = raw_input("Enter the url: ")
-field = raw_input("Enter the field: ")
-term = raw_input("Enter the term: ")
-url = url + '/_search'
+	url = raw_input("Enter the url: ")
+	field = raw_input("Enter the field: ")
+	term = raw_input("Enter the term: ")
+	url = url + '/_search'
 
-#executing the functions
-format(search(url, field, term))
+	#executing the functions
+	format(search(url, field, term))
+
+if a==1:
+	url = raw_input("Enter the url: ")
+	id = raw_input("Enter id: ")
+	s = 'curl -XGET '+url+'/'+  id
+	os.system(s)
