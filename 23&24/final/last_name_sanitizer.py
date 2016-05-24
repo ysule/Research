1ea7551fea815ada0e0c7d6befb2948bdd0c3fc2
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-05-24 16:26:51 +0530

    Janatha Garage

--- 23&24/final/last_name_sanitizer.py
+++ 23&24/final/last_name_sanitizer.py
@@ -13,10 +13,10 @@ import string
 client = MongoClient()
 
 #edit the below line to set the database.
-#db = client.database_name
+db = client.IVF_data
 
 #edit the following line to set the collection name
-#cursor_lname = db.collection_name.find()
+cursor_lname = db.ivf_clinical_trails_12052016.find()
 
 for result_object in cursor_lname:
 	#we are exluding result_objects with no 'investigators' key by using the following if condition
@@ -25,16 +25,19 @@ for result_object in cursor_lname:
 		#here please note that a len(result_object['investigators'] gives use the number of last_name keys in eac investigator key
 		i=0
 		while i<len(result_object['investigators']):
-		#here head gives use the value before ","
-			head, sep, tail = result_object['investigators'][i]['last_name'].partition(',')
-		#as in each value of the key 'last_name' after "," there are unnecessary characters, we need the string before ","
-			result_object['investigators'][i]['last_name'] = head
-			result_object['investigators'][i]['last_name'] = result_object['investigators'][i]['last_name'].replace("-"," ")
+			try:
+				#here head gives use the value before ","
+				head, sep, tail = result_object['investigators'][i]['last_name'].partition(',')
+				#as in each value of the key 'last_name' after "," there are unnecessary characters, we need the string before ","
+				result_object['investigators'][i]['last_name'] = head
+				print head
+				result_object['investigators'][i]['last_name'] = result_object['investigators'][i]['last_name'].replace("-"," ")
+			except KeyError:
+				print' '
+				
 			i = i+1
-	else:
-		result_object = result_object
 	#THIS IS IMPORTANT
 	#HERE WRITE THE MONGO OPERATION YOU NEED
 	#IN THE GIVEN EXAMPLE BELOW the output is being inserted into a collection named "praneeth_i"
-	#IF YOU NEED TO UPDATE ORIGINAL TABLE, TAKE A BACKUP OF IT FIRST AND CHANGE TO FOLLOWING LINE TO update
-	#db.praneeth_i.insert(result_object)
+	#IF YOU NEED TO UPDATE ORIGINAL TABLE, TAKE A BACKUPa OF IT FIRST AND CHANGE TO FOLLOWING LINE TO update
+	db.praneeth_clinical.insert(result_object)
