c65d8d56eea2a4b24d618e45e7b8030671876e3f
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-05-24 17:30:18 +0530

    Janatha Garage

---
+++ 23&24/final(backup)/final/phase_rename.py
@@ -0,0 +1,29 @@
+#This script changes 1,2,3,4 in phase names to roman numbers
+import pymongo
+import os
+from pymongo import MongoClient
+import json
+import requests
+import urllib
+import time
+from time import sleep
+import string
+
+client = MongoClient()
+
+db = client.IVF_data
+
+cursor_lname = db.ivf_clinical_trails_12052016.find()
+
+for result_object in cursor_lname:
+	if('phase' in result_object):
+		if result_object['phase'] =='Phase 4':
+			result_object['phase'] ='Phase IV'
+		if result_object['phase'] =='Phase 3':
+                        result_object['phase'] ='Phase III'
+		if result_object['phase'] =='Phase 2':
+                        result_object['phase'] ='Phase II'
+		if result_object['phase'] =='Phase 1':
+                        result_object['phase'] ='Phase I'
+		print result_object['phase']
+	#db.praneeth_phase.insert(result_object)
