fc4dde4a1e79f0d14c6868aaaa5572efe047938a
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-05-24 15:54:19 +0530

    Janatha Garage

---
+++ 23&24/final/last_name_sanitizer.py
@@ -0,0 +1,40 @@
+#This script sanitizes the last_name in investigators key i.e: Removes "," and "MD","PhD" etc and removes "-"
+#If an error shows up after the script runs, ignore it. It doesn't change anything.
+import pymongo
+import os
+from pymongo import MongoClient
+import json
+import requests
+import urllib
+import time
+from time import sleep
+import string
+
+client = MongoClient()
+
+#edit the below line to set the database.
+#db = client.database_name
+
+#edit the following line to set the collection name
+#cursor_lname = db.collection_name.find()
+
+for result_object in cursor_lname:
+	#we are exluding result_objects with no 'investigators' key by using the following if condition
+	if 'investigators' in result_object:
+		#as each 'investigators' key may have more than one 'last_name' we are writing a loop to iterate over the list and select the last_name
+		#here please note that a len(result_object['investigators'] gives use the number of last_name keys in eac investigator key
+		i=0
+		while i<len(result_object['investigators']):
+		#here head gives use the value before ","
+			head, sep, tail = result_object['investigators'][i]['last_name'].partition(',')
+		#as in each value of the key 'last_name' after "," there are unnecessary characters, we need the string before ","
+			result_object['investigators'][i]['last_name'] = head
+			result_object['investigators'][i]['last_name'] = result_object['investigators'][i]['last_name'].replace("-"," ")
+			i = i+1
+	else:
+		result_object = result_object
+	#THIS IS IMPORTANT
+	#HERE WRITE THE MONGO OPERATION YOU NEED
+	#IN THE GIVEN EXAMPLE BELOW the output is being inserted into a collection named "praneeth_i"
+	#IF YOU NEED TO UPDATE ORIGINAL TABLE, TAKE A BACKUP OF IT FIRST AND CHANGE TO FOLLOWING LINE TO update
+	#db.praneeth_i.insert(result_object)
---
+++ 23&24/final/merc.sharp_rename.py
@@ -0,0 +1,30 @@
+#This script renames Merc sharp ... in sponsors to MSD
+import pymongo
+import os
+from pymongo import MongoClient
+import json
+import requests
+import urllib
+import time
+from time import sleep
+import string
+
+client = MongoClient()
+
+#db = client.IVF_data
+
+#cursor_lname = db.praneeth.find()
+
+for result_object in cursor_lname:
+	if('sponsors' in result_object):
+		i = 0
+		while i < len(result_object['sponsors']):
+			if "Merck sharp" in result_object['sponsors'][i]['sponsor']:
+				print result_object['sponsors'][i]['sponsor']
+				print 'is changed to'
+				result_object['sponsors'][i]['sponsor'] = 'MSD'
+				print result_object['sponsors'][i]['sponsor']
+			else:
+				print 'not changed'
+			i = i+1
+	#db.collection.insert(result_object)
