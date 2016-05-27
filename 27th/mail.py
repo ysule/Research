a18173341eb3a70302309ef3b8f296a39367ba32
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-05-27 12:17:41 +0530

    Janatha Garage

---
+++ 27th/mail.py
@@ -0,0 +1,24 @@
+import smtplib
+from random import randint
+fromaddr = 'praneethbedapudi@gmail.com'
+toaddrs  = 'bedapudi.praneeth@innoplexus.com'
+
+otp_number = randint(10000,99999)
+print "OTP: ",otp_number
+
+msg = "\r\n".join([
+  "From: praneethbedapudi@gmail.com",
+  "To: bedapudi.praneeth@gmail.com",
+  "Subject: Hello",
+  "",
+  str(otp_number)
+  ])
+
+username = 'praneethbedapudi@gmail.com'
+password = 'rao!pravam3'
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
