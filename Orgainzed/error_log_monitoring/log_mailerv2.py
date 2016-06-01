630fc6123c5a7ec6d7d677987a933766da3e8b54
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-01 12:19:57 +0530

    Janatha Garage

--- Orgainzed/error_log_monitoring/log_mailerv2.py
+++ Orgainzed/error_log_monitoring/log_mailerv2.py
@@ -24,33 +24,34 @@ while 1:
 		for line in lines:
 			print line
 			if find_str in line:
-				fromaddr = "praneethbedapudi@gmail.com"
-				toaddr = "bedapudi.praneeth@innoplexus.com"
-				 
-				msg = MIMEMultipart()
-				 
-				msg['From'] = fromaddr
-				msg['To'] = toaddr
-				msg['Subject'] = "error log"
-				 
-				body = "error log attached"
-				 
-				msg.attach(MIMEText(body, 'plain'))
-				 
-				filename = "error.log"
-				attachment = open("/var/log/nginx/error.log", "rb")
-				 
-				part = MIMEBase('application', 'octet-stream')
-				part.set_payload((attachment).read())
-				encoders.encode_base64(part)
-				part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-				 
-				msg.attach(part)
-				 
-				server = smtplib.SMTP('smtp.gmail.com', 587)
-				server.starttls()
-				server.login(fromaddr, "rao!pravam3")
-				text = msg.as_string()
-				server.sendmail(fromaddr, toaddr, text)
-				server.quit()
-				print'Mail sent!!'
+				fromaddr = "serverstatus@app.innoplexus.de"
+				toaddr = ["suyash.masugade@innoplexus.com","bedapudi.praneeth@innoplexus.com"]
+				for t in toaddr:
+					msg = MIMEMultipart()
+					 
+					msg['From'] = fromaddr
+					msg['To'] = t
+					msg['Subject'] = "error log"
+					 
+					body = "error log attached"
+					 
+					msg.attach(MIMEText(body, 'plain'))
+					 
+					filename = "error.log"
+					attachment = open("/var/log/nginx/error.log", "rb")
+					 
+					part = MIMEBase('application', 'octet-stream')
+					part.set_payload((attachment).read())
+					encoders.encode_base64(part)
+					part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+					 
+					msg.attach(part)
+					
+					server = smtplib.SMTP('email-smtp.us-east-1.amazonaws.com:587')
+					server.starttls()
+					server.login("AKIAJLVT63DSM247NJHQ", "AvAqwJImHHr9Ow98fImQo9E4GvI73WiKQgsSKAGfId70")
+					text = msg.as_string()
+					server.sendmail(fromaddr, t, text)
+					server.quit()
+				print'Mails sent!!'
+				sum_start = sum_now
