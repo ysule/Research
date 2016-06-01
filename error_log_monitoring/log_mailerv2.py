c0bbe895f1dba19d905b4bfead778dee001ae71d
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-01 12:36:03 +0530

    Bedapudi Praneeth

--- error_log_monitoring/log_mailerv2.py
+++ error_log_monitoring/log_mailerv2.py
@@ -26,25 +26,32 @@ while 1:
 			if find_str in line:
 				fromaddr = "serverstatus@app.innoplexus.de"
 				toaddr = ["suyash.masugade@innoplexus.com","bedapudi.praneeth@innoplexus.com"]
-				server = smtplib.SMTP('email-smtp.us-east-1.amazonaws.com:587')
-				server.starttls()
-				server.login("AKIAJLVT63DSM247NJHQ", "AvAqwJImHHr9Ow98fImQo9E4GvI73WiKQgsSKAGfId70")
 				for t in toaddr:
 					msg = MIMEMultipart()
+					 
 					msg['From'] = fromaddr
 					msg['To'] = t
 					msg['Subject'] = "error log"
+					 
 					body = "error log attached"
+					 
 					msg.attach(MIMEText(body, 'plain'))
+					 
 					filename = "error.log"
 					attachment = open("/var/log/nginx/error.log", "rb")
+					 
 					part = MIMEBase('application', 'octet-stream')
 					part.set_payload((attachment).read())
 					encoders.encode_base64(part)
 					part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+					 
 					msg.attach(part)
+					
+					server = smtplib.SMTP('email-smtp.us-east-1.amazonaws.com:587')
+					server.starttls()
+					server.login("AKIAJLVT63DSM247NJHQ", "AvAqwJImHHr9Ow98fImQo9E4GvI73WiKQgsSKAGfId70")
 					text = msg.as_string()
 					server.sendmail(fromaddr, t, text)
-				server.quit()
+					server.quit()
 				print'Mails sent!!'
 				sum_start = sum_now
