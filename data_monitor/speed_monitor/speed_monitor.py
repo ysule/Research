93c738c77f76a14efc71fe00846dc5ef49158083
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-03 17:28:55 +0530

    Bedapudi Praneeth

--- data_monitor/speed_monitor/speed_monitor.py
+++ data_monitor/speed_monitor/speed_monitor.py
@@ -1,6 +1,15 @@
 import string
 with open('/home/praneeth/Desktop/test.log') as f:
 	lines = f.readlines()
+list = ''
+list_m = ''	
 for line in lines:
 	if 'from 192.168.1.56' in line:
-		print line
+		for s in line.split():
+			if s.isdigit():
+				if(int(s)>100):
+					list = list+ (line.split("to ",1)[1]).split(":",1)[0] + '\n'
+					if list.count((line.split("to ",1)[1]).split(":",1)[0]) > 5:
+						if((line.split("to ",1)[1]).split(":",1)[0] not in list_m):
+							list_m = list_m + (line.split("to ",1)[1]).split(":",1)[0] + '\n'
+print list_m
