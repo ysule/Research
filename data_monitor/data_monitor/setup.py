5e4c591e556b56212d033a8bfd9edf7bf4c4def5
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-06 16:33:21 +0530

    Bedapudi Praneeth

--- data_monitor/data_monitor/setup.py
+++ data_monitor/data_monitor/setup.py
@@ -9,7 +9,7 @@ os.system('sudo pip install paramiko')
 os.system('sudo pip install xlwt')
 os.system('wget https://raw.githubusercontent.com/bedapudi6788/Python-scripts/master/data_monitor/data_monitor.tar.gz')
 os.system('tar -xzf data_monitor.tar.gz')
-os.system('(crontab -l ; echo "nohup python '+path+data_monitor.py+'")| crontab -')
+os.system('(crontab -l 2>/dev/null; echo "@reboot nohup python '+path+'data_monitor.py") | crontab -')
 
 print 'Run server_add.py to add servers and their passwords'
 print 'PLEASE DO NOT MANUALLY ADD ANYTHING TO servers.txt FILE'
