0e4bc65e5af794dd1fefdc9c74bac2311c0328ba
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-06 11:46:04 +0530

    Bedapudi Praneeth

--- data_monitor/data_monitor/setup.py
+++ data_monitor/data_monitor/setup.py
@@ -2,6 +2,7 @@ import os
 
 os.system('sudo pip install paramiko')
 os.system('sudo pip install xlwt')
+os.system('wget https://raw.githubusercontent.com/bedapudi6788/Python-scripts/master/data_monitor/data_monitor.tar.gz')
 os.system('tar -xzf data_monitor.tar.gz')
 
 print 'Run server_add.py to add servers and their passwords'
