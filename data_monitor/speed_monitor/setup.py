500afc80703a03a14ec411884a9b3a0ca64bd59d
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-06 17:14:43 +0530

    Bedapudi Praneeth

--- data_monitor/speed_monitor/setup.py
+++ data_monitor/speed_monitor/setup.py
@@ -1,11 +1,14 @@
 #Run this on each server computer
 #Install iptraf on each server "sudo apt-get install iptraf"
 import os
-
+import sys
+path = os.path.dirname(os.path.realpath(sys.argv[0]))
+path = path+'/speed_monitor/'
+os.system('sudo pip install paramiko')
 os.system('sudo pip install netifaces')
-os.system('wget https://raw.githubusercontent.com/bedapudi6788/Python-scripts/master/data_monitor/data_monitor.tar.gz')
+os.system('wget https://raw.githubusercontent.com/bedapudi6788/Python-scripts/master/data_monitor/speed_monitor.tar.gz')
 os.system('tar -xzf data_monitor.tar.gz')
-os.system('(crontab -l 2>/dev/null; echo "@reboot nohup python '+path+'data_monitor.py &") | crontab -')
+os.system('(crontab -l 2>/dev/null; echo "@reboot nohup python '+path+'speed_monitor_v1.2.py &") | crontab -')
 
 print 'Run server_add.py to add servers and their passwords'
 print 'PLEASE DO NOT MANUALLY ADD ANYTHING TO servers.txt FILE'
