5bb92ff3bb85d63d3635b0ac4c28228dc62511d2
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-07 17:06:53 +0530

    Bedapudi Praneeth

--- text_corrector/universities.py
+++ text_corrector/universities.py
@@ -1,6 +1,11 @@
 with open('./universities.txt') as f:
 	uni = f.readlines()
-for uni in f:
-	uni = uni.replace("\n")
-	uni = uni.split(",",1)[1]
-	uni = uni.split(",",1)[0]
+for u in uni:
+	u = u.replace("\n","")
+	u = u.split(",",1)[1]
+	u = u.split(",",1)[0]
+	words = u.split()
+	for word in words:
+		o1 = open("./uni_words.txt","a")
+		o1.write(word+"\n")
+		o1.close
