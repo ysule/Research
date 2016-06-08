cda3c0f5a9356a5f98820d94323aaa3a8a1e7620
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-08 10:30:19 +0530

    Bedapudi Praneeth

--- text_corrector/text_corrector_not_wanted.py
+++ text_corrector/text_corrector_not_wanted.py
@@ -1,29 +1,29 @@
 def corrector(line, not_wanted):
-	x = ''
+	corrected_line = ''
 	for character in line:
 		if character in not_wanted:
 			character = ''
 		else:
-			x = x + character
-	words = x.split()
-	x = ''
+			corrected_line = corrected_line + character
+	words = corrected_line.split()
+	corrected_line = ''
 	for word in words:
 		letter_count = 0
 		for letter in word:
-			if letter in 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890':
+			if letter in 'qwertyuiopasdfghjklzcorrected_linecvbnmQWERTYUIOPASDFGHJKLZcorrected_lineCVBNM1234567890':
 				letter_count = letter_count + 1
-		if letter_count > (len(word)/2): 
-			if(len(word) == 1):
+		if letter_count > (len(word)/2):
+			if len(word) == 1:
 				if word in 'aAiI,.':
-					x = x +' ' + word
+					corrected_line = corrected_line +' ' + word
 				else:
 					word = ''
 			else:
-				x = x + ' ' + word
-	print x
+				corrected_line = corrected_line + ' ' + word
+	print corrected_line
 #not_wanted = raw_input("not wanted:")
 #not_wanted = '~`!@#$%^&*()_+=[]{}\\/'
-with open('./not_wanted.txt') as file:
-	not_wanted=file.read().replace('\n', '')
+with open('./not_wanted.txt') as Myfile:
+	not_wanted_list = Myfile.read().replace('\n', '')
 while 1:
-	corrector(raw_input("line: "), not_wanted)
+	corrector(raw_input("line: "), not_wanted_list)
