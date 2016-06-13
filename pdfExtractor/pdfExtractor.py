0f7be9025bf040eedbbfddb723376c48821d5076
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-13 16:22:56 +0530

    Bedapudi Praneeth

--- pdfExtractor/pdfExtractor.py
+++ pdfExtractor/pdfExtractor.py
@@ -1,47 +1,58 @@
 import os
+import pymongo
+import ast
+from pymongo import MongoClient
+client = MongoClient()
+db = client.dummy
+def is_empty(x):
+    n = 0
+    s = 'qwertyuiopasdfghjklzxcvbnm0123456789'
+    for i in s:
+        if i in x:
+            n = n+1
+    if n>0:
+        return 0
+    else:
+        return 1
 #getting the names of all pdf files in the current folder
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 files = filter(lambda f: f.endswith(('.pdf')), files)
 for pdf_file in files:
-    #checking and creating html file for pdf file if html file is not there
-    html_files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    html_files = filter(lambda f: f.endswith(('.html')), html_files)
-    html_file = pdf_file.replace('.pdf','') + '.html'
-    if html_file not in html_files:
-        os.system("pdf2txt.py -o " + pdf_file.replace('.pdf','') + '.html -t html '+pdf_file)
-#getting the html files names (in the folder) after generating html files for pdfs
-html_files = [f for f in os.listdir('.') if os.path.isfile(f)]
-html_files = filter(lambda f: f.endswith(('.html')), html_files)
-for html_file in html_files:
-    with open(html_file) as f:
+    #checking and creating text file for pdf file if text file is not there
+    text_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    text_files = filter(lambda f: f.endswith(('.txt')), text_files)
+    text_file = pdf_file.replace('.pdf','') + '.txt'
+    if text_file not in text_files:
+        os.system("pdf2txt.py -o " + pdf_file.replace('.pdf','') + '.txt -t text '+pdf_file)
+#getting the text files names (in the folder) after generating text files for pdfs
+text_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+text_files = filter(lambda f: f.endswith(('.txt')), text_files)
+for text_file in text_files:
+    with open(text_file) as f:
         lines = f.readlines()
         f.close()
-    """
-    with open(html_file) as f:
-        all_lines = f.read()
-        f.close()
-    """
-#finding the element with max font size in the first 30 lines and extracting title from it.
-    font_size_old = 0
-    for i,line in enumerate(lines):
-        if i<30:
-            if 'font-size:' in line:
-                x = line.split("font-size:",1)[1]
-                font_size_new = int(x.split("px",1)[0])
-                if font_size_new > font_size_old:
-                    title = line.rsplit('">', 1)[-1].replace("\n","")
-                    font_size_old = font_size_new
-                    if 'font-size' not in lines[i+1]:
-                        if '<br>' in lines[i+1]:
-                            if '<' or '>' not in lines[i+1].replace('<br>',''):
-                                title = title + lines[i+1].replace('<br','')
-    if title:
-        print 'title: '+title
-    """
-    if 'abstract' in all_lines.lower():
-        x = all_lines.split("Abstract",1)[1]
-        y = x.split("style",1)[1]
-        z = y.split("style",1)[0]
-    if z:
-        print 'abstract version of abstract is ' + z
-    """
+    list_empty_lines = 0
+    x = list('')
+    temp = list('')
+    for a in lines:
+        if is_empty(a):
+            x.append(temp)
+            temp = list('')
+        else:
+            temp.append(a)
+    '''
+    while 1:
+        print x[int(raw_input("aa:"))]
+    '''
+    for i,b in enumerate(x):
+        if len(b)>0:
+            if ', ' in b[0]:
+                if len(b)>1:
+                    affliation = ''
+                    for k,p in enumerate(b):
+                        if k>0:
+                            affliation = affliation + p
+                    #print affliation + '==================='
+                    insert_data = '{"name":"'+str(b[0].replace('\n',''))+'","affiliation":"'+affliation+'"}'
+                    insert_data = insert_data.replace('\n',' ')
+                    db.dummy.insert(ast.literal_eval(insert_data))
---
+++ pdfExtractor/try-1/pdfExtractor.py
@@ -0,0 +1,47 @@
+import os
+#getting the names of all pdf files in the current folder
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+files = filter(lambda f: f.endswith(('.pdf')), files)
+for pdf_file in files:
+    #checking and creating html file for pdf file if html file is not there
+    html_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    html_files = filter(lambda f: f.endswith(('.html')), html_files)
+    html_file = pdf_file.replace('.pdf','') + '.html'
+    if html_file not in html_files:
+        os.system("pdf2txt.py -o " + pdf_file.replace('.pdf','') + '.html -t html '+pdf_file)
+#getting the html files names (in the folder) after generating html files for pdfs
+html_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+html_files = filter(lambda f: f.endswith(('.html')), html_files)
+for html_file in html_files:
+    with open(html_file) as f:
+        lines = f.readlines()
+        f.close()
+    """
+    with open(html_file) as f:
+        all_lines = f.read()
+        f.close()
+    """
+#finding the element with max font size in the first 30 lines and extracting title from it.
+    font_size_old = 0
+    for i,line in enumerate(lines):
+        if i<30:
+            if 'font-size:' in line:
+                x = line.split("font-size:",1)[1]
+                font_size_new = int(x.split("px",1)[0])
+                if font_size_new > font_size_old:
+                    title = line.rsplit('">', 1)[-1].replace("\n","")
+                    font_size_old = font_size_new
+                    if 'font-size' not in lines[i+1]:
+                        if '<br>' in lines[i+1]:
+                            if '<' or '>' not in lines[i+1].replace('<br>',''):
+                                title = title + lines[i+1].replace('<br','')
+    if title:
+        print 'title: '+title
+    """
+    if 'abstract' in all_lines.lower():
+        x = all_lines.split("Abstract",1)[1]
+        y = x.split("style",1)[1]
+        z = y.split("style",1)[0]
+    if z:
+        print 'abstract version of abstract is ' + z
+    """
