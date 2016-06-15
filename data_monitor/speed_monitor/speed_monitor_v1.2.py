90ee1f8025f24d5d8ddbaaf9b3baff1446889025
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-15 15:35:53 +0530

    Bedapudi Praneeth

--- data_monitor/speed_monitor/speed_monitor_v1.2.py
+++ data_monitor/speed_monitor/speed_monitor_v1.2.py
@@ -13,18 +13,21 @@ from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
 
+#While deploying on server comment or change the following line to server's ip
 from_ip = '192.168.1.56'
-#ni.ifaddresses('eth0')[2][0]['addr']
+#While deploying on server change eth0 in the following line to whatever the interface name is and uncomment it
+#from_ip = ni.ifaddresses('eth0')[2][0]['addr']
 statement = 'from ' + from_ip
 date_old = time.strftime("%x")
 while 1:
 	date_now = time.strftime("%x")
+	#creating new log for each day
 	name = date_now + '.log'
 	name = string.replace(name, '/', '-')
 	#os.system('rm ./'+name)
 	os.system('touch ./'+name)
-	#command = 'sudo iptraf -i all -L ./'+name+'-t 1430 -B'
-	command = 'sudo iptraf -i all -L ./'+name+' -B'
+	command = 'sudo iptraf -i all -L ./'+name+'-t 1430 -B'
+	#command = 'sudo iptraf -i all -L ./'+name+' -B'
 	os.system(command)
 	time_old = int(time.strftime('%X')[:2]) - 1
 	#list_m_old = ''
@@ -43,7 +46,7 @@ while 1:
 				if statement in line:
 					for s in line.split():
 						if s.isdigit():
-							#change 100 in the following line to minimum thershold value
+			#While deploying change 100 in the following line to minimum thershold value
 							if(int(s)>100):
 								print s
 								list = list+ (line.split("to ",1)[1]).split(":",1)[0] + '\n'
@@ -63,8 +66,8 @@ while 1:
 			print list_m
 			list_m_old = list_m
 			"""
-			print 'list'
-			print list_m
+			#print 'list'
+			#print list_m
 			#Mailing
 			fromaddr = "serverstatus@app.innoplexus.de"
 			toaddr = ["suyash.masugade@innoplexus.com","bedapudi.praneeth@innoplexus.com","pradumna.panditrao@innoplexus.com"]
