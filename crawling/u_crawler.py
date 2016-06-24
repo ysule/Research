2e0aa54f2da9575bb8969f8a9eaadb53808fca0a
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-24 11:12:51 +0530

    Bedapudi Praneeth

--- crawling/u_crawler.py
+++ crawling/u_crawler.py
@@ -1,9 +1,12 @@
 from nltk.tag import pos_tag
 
-sentence = "Michael Jackson likes to eat at McDonalds"
+sentence = "John Connor is a very bad movie. Marvel Civil War is far better than that. Dr. Who rocks. Hate"
 tagged_sent = pos_tag(sentence.split())
-# [('Michael', 'NNP'), ('Jackson', 'NNP'), ('likes', 'VBZ'), ('to', 'TO'), ('eat', 'VB'), ('at', 'IN'), ('McDonalds', 'NNP')]
-
-propernouns = [word for word,pos in tagged_sent if pos == 'NNP']
-# ['Michael','Jackson', 'McDonalds']
-print propernouns
+names = [word for word,pos in tagged_sent if pos == 'NNP']
+all_words = sentence.split(' ')
+for i,word in enumerate(all_words):
+    try:
+        if all_words[i-1] in names and all_words[i] in names:
+            print all_words[i-1] + ' ' + all_words[i] +'\n'
+    except:
+        x = 1
