6197fcc2f68f826cac1f6844589156b99fee6af9
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-06-25 10:49:37 +0530

    Bedapudi Praneeth

--- Scoring_Touchpoint/social_scorer.py
+++ Scoring_Touchpoint/social_scorer.py
@@ -1,3 +1,12 @@
+#likes -1
+#comments -10
+#shares -5
+#First scores are calculated by above weightages
+#Scores are divided by max possible score
+#Scores are divided by sum of score
+#Avg is calculated
+#Above steps are repeated for facebook and youtube
+#Scores are divided by sum of averages (of facebook and youtube)
 import pymongo
 from pymongo import MongoClient
 import json
@@ -12,15 +21,15 @@ f_no['s'] = list('')
 for f_i,f_result in enumerate(f_cursor):
     f_data[f_i] = {}
     try:
-        f_no['l'].append(len(f_result['likes']['data']))
+        f_no['l'].append(1*len(f_result['likes']['data']))
     except:
         f_no['l'].append(0)
     try:
-        f_no['c'].append(len(f_result['comments']))
+        f_no['c'].append(5*len(f_result['comments']))
     except:
         f_no['c'].append(0)
     try:
-        f_no['s'].append((f_result['shares']['count']))
+        f_no['s'].append(10*(f_result['shares']['count']))
     except:
         f_no['s'].append(0)
     f_data[f_i] = f_result
@@ -37,15 +46,15 @@ y_no['s'] = list('')
 for y_i,y_result in enumerate(y_cursor):
     y_data[y_i] = {}
     try:
-        y_no['l'].append(len(y_result['likes']['data']))
+        y_no['l'].append(1*len(y_result['likes']['data']))
     except:
         y_no['l'].append(0)
     try:
-        y_no['c'].append(len(y_result['comments']))
+        y_no['c'].append(5*len(y_result['comments']))
     except:
         y_no['c'].append(0)
     try:
-        y_no['s'].append((y_result['shares']['count']))
+        y_no['s'].append(10*(y_result['shares']['count']))
     except:
         y_no['s'].append(0)
     y_data[y_i] = y_result
@@ -67,8 +76,14 @@ total_avg = f_avg + y_avg
 db.endo_blog_data.remove({})
 
 for i in range(0,f_i):
+    #dividing scores by sum of scores
+    f_data[i]['score'] = f_data[i]['score']/f_total
+    #dividing scores by sum of averages of scores
     f_data[i]['score'] = f_data[i]['score']/total_avg
     db.endo_blog_data.insert(f_data[i])
 for i in range(0,y_i):
+    #dividing scores by sum of scores
+    y_data[i]['score'] = y_data[i]['score']/y_total
+    #dividing scores by sum of averages of scores
     y_data[i]['score'] = y_data[i]['score']/total_avg
     db.endo_blog_data.insert(y_data[i])
