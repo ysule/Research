7d40c78cb06ccd2b68494cfc943f5164ec8dd492
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-07-04 13:40:14 +0530

    Bedapudi Praneeth

--- OTP/PHP & Python files/connection.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbc = mysql_connect('localhost','root','') or  die("Cant connect :" . mysql_error());
- 
-mysql_select_db("test-login",$dbc)
- 
-or
-die("Cant connect :" . mysql_error()); 
- 
-?>
--- OTP/PHP & Python files/mail.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$command = escapeshellcmd('python test.py');
-$output = shell_exec($command); echo ' output:';
-echo $output;
-?>
