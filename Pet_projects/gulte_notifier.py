e2e680e4d5b20b3b9c2e9bff5e61025a0d95a6a3
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-07-04 15:35:31 +0530

    Bedapudi Praneeth

--- Pet_projects/gulte_notifier.py
+++ Pet_projects/gulte_notifier.py
@@ -11,10 +11,21 @@ with open('index.php') as f:
     data = f.read()
 title = latest_news(data).split('"',1)[1]
 title = title.split('"',1)[0]
-link = latest_news(data).split('" ',1)[1]
-link = link.split(' ',1)[0]
-print title
-print link
+link = latest_news(data).split('href="',1)[1]
+link = link.split('"',1)[0]
 pynotify.init("Test")
 notice = pynotify.Notification(title, link)
 notice.show()
+while 1:
+    os.system('rm index.php')
+    os.system('wget www.gulte.com/index.php')
+    with open('index.php') as f:
+        data = f.read()
+    title_new = latest_news(data).split('"',1)[1]
+    title_new = title_new.split('"',1)[0]
+    link_new = latest_news(data).split('href="',1)[1]
+    link_new = link_new.split('"',1)[0]
+    if title_new!=title:
+        pynotify.init("Test")
+        notice = pynotify.Notification(title_new, link_new)
+        notice.show()
