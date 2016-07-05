9b44524b0438986e490daf63ea50ec0225277481
bedapudi6788 <bedapudipraneeth@outlook.com>
2016-07-05 15:17:08 +0530

    Bedapudi Praneeth

--- Bulk indexing/bulk_indexer_with_count_v1.1.py
+++ Bulk indexing/bulk_indexer_with_count_v1.1.py
@@ -6,8 +6,11 @@ import json
 import requests
 import urllib
 import time
+import sys
+from sys import getsizeof as size
 
 start_at = int(raw_input("Enter where you want to start at: (Enter 0 for default)"))
+bulk_size = int(raw_input("Enter bulk object size: "))
 start_time = time.time()
 client = MongoClient()
 
@@ -48,16 +51,17 @@ os.system(c)
 
 bulk_body = ''
 cursor = db.normalized_author_list.find({},{'_id':0,'Original affiliation name':1,'Normalized affiliation name':1})
-
+old_count = 0
 for count,result_object in enumerate(cursor):
     if count>=start_at:
         try:
         	bulk_body = bulk_body + '{ "index" : { "_index" : "normalizedaffiliation", "_type" : "micro"} }\n'
         	bulk_body = bulk_body + json.dumps(result_object)+'\n'
-        	if (count+1)%1000 == 0:
+        	if size(bulk_body)>1000000*bulk_size == 0:
         		client.bulk(body=bulk_body)
-        		print 'sent the' + str(count/1000) + 'th 1000 documents'
+        		print 'sent ' + str(count - old_count) + ' documents'
         		bulk_body = ''
+                old_count = count
         except:
             print 'stopped at '+str(count)
             exit()
